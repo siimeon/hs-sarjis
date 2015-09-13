98f779adc2a15ca0a95cfd6a6ecab0701a93a515
siimeon <simo.haakana@aalto.fi>
2015-09-13 21:37:45 +0300

    Adding working and very simple AngularJS app to show comic

--- comicPartser/test_comicParserCli.py
+++ comicPartser/test_comicParserCli.py
@@ -16,6 +16,14 @@ class TestHtmlParser(unittest.TestCase):
         partial_hit = "snstatic.fi/webkuva/taysi/1920"
         self.assertIn(partial_hit, parser.search())
 
+    def test_iltalehti_fingerpori(self):
+        url = "http://www.iltalehti.fi/fingerpori/"
+        search_term = "iltalehti.fi/sarjakuvat/"
+        parser = HtmlAttributeParser(url=url,
+                                     search_term=search_term)
+        partial_hit = "iltalehti.fi/sarjakuvat/Fingerpori"
+        self.assertIn(partial_hit, parser.search())
+
 
 if __name__ == "__main__":
     unittest.main()
